0259791692e8f55919e53aa538dc9394caba7382
Saulo Grego <saulogrego@gmail.com>
2018-10-06 15:49:09 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/tcc/TccstenioApplication.java
@@ -0,0 +1,12 @@
+package com.tcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccstenioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccstenioApplication.class, args);
+	}
+}
